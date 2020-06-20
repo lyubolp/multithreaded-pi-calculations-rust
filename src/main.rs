41193f18c901d63eef8a5f47d6c1c0ae3bb1c51c
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-06-20 12:37:08 +0300

    Finished work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod calc;
 
-use crate::calc::pi_calculator::{calculate_a_n_from_previous, calculate_a_n_from_formula};
+use crate::calc::pi_calculator::{Task, DEFAULT_PRECISION};
 use rug::{Float};
 use std::env;
 use std::thread;
@@ -8,20 +8,13 @@ use std::thread::JoinHandle;
 use std::collections::VecDeque;
 use std::sync::{mpsc};
 use std::ptr::null;
+use std::sync::mpsc::{Sender, Receiver};
+
 
 fn handle_args(args: Vec<String>) -> (usize, usize)
 {
-    let precision_default: u32 = 10000;
-    //let max_thread = num_cpus::get();
-    let max_thread = 2;
-    /*let max_thread = match cpuid::identify() {
-        Ok(info) => info.num_logical_cpus,
-        Err(err) => {
-            println!("Error getting cpu info, default is 1");
-            1
-        }
-    };*/
-
+    let mut max_thread = num_cpus::get();
+    max_thread = 2;
     let mut result = (0usize, 0usize);
     if args.len() == 5 {
         if args[1] == String::from("-p")
@@ -30,7 +23,7 @@ fn handle_args(args: Vec<String>) -> (usize, usize)
                 Ok(value) => result.0 = value,
                 Err(_) => {
                     println!("Invalid argument for -p, setting it to default");
-                    result.0 = precision_default as usize;
+                    result.0 = DEFAULT_PRECISION as usize;
                 }
             }
         }
@@ -51,107 +44,173 @@ fn handle_args(args: Vec<String>) -> (usize, usize)
                 Ok(value) => result.0 = value,
                 Err(_) => {
                     println!("Invalid argument for -p, setting it to default");
-                    result.0 = precision_default as usize;
+                    result.0 = DEFAULT_PRECISION as usize;
                 }
             }
         }
         result.1 = max_thread;
     } else {
-        println!("Invalid command line arguments, setting the default values (-p {} -t {})", precision_default, max_thread);
-        result.0 = precision_default as usize;
+        println!("Invalid command line arguments, setting the default values (-p {} -t {})", DEFAULT_PRECISION, max_thread);
+        result.0 = DEFAULT_PRECISION as usize;
         result.1 = max_thread;
     }
     result
 }
 
 fn main() {
-    let default_precision: u32 = 10000;
     let (amount_of_elements, thread_count) = handle_args(env::args().collect());
-
     println!("Starting the program with {} elements to be calculated on {} threads", amount_of_elements, thread_count);
 
-    let mut result = Float::with_val(default_precision, 0);
-    let mut elements:Vec<Float> = vec![Float::new(default_precision); amount_of_elements as usize];
-    let mut tasks: Vec<VecDeque<u32>> = vec![VecDeque::new(); thread_count];
-
-    let amount_of_work_per_thread = {
-        if amount_of_elements % thread_count != 0{
-            (amount_of_elements / thread_count) + 1
-        } else {
-            (amount_of_elements / thread_count)
-        }
-    };
+    let mut result = Float::with_val(DEFAULT_PRECISION, 0);
 
 
+    let mut task_tuples: VecDeque<(u32, u32)> = VecDeque::new();
+    let amount_of_work_per_thread = (amount_of_elements / thread_count) / 2;
 
-    let mut last_calculated_index: Vec<u32> = vec![0; thread_count];
-    let mut calculated_elements: u32 = 0;
-
-    for current_thread in 0..thread_count{
-        elements[(amount_of_work_per_thread * current_thread) as usize] =
-            calculate_a_n_from_formula( (amount_of_work_per_thread * current_thread) as u32,default_precision);
-
-        last_calculated_index[current_thread] = (amount_of_work_per_thread * current_thread) as u32;
-        calculated_elements += 1;
-        result += elements[(amount_of_work_per_thread * current_thread) as usize].clone();
-    }
-
-    for current_thread in 0..thread_count{
-        for i in 1..amount_of_work_per_thread{
-            tasks[current_thread as usize].push_back(((amount_of_work_per_thread * current_thread) as u32 + i as u32));
-        }
-    }
-
-    let mut threads_handles: Vec<JoinHandle<()>> = vec!();
-    let mut channels: Vec<mpsc::Receiver<(Float, u32)>> = vec!();
-    for current_thread in 0..thread_count{
-        let (sender, receiver) = mpsc::channel();
-        channels.push(receiver);
-
-        let previous_element = elements[(current_thread * amount_of_work_per_thread) as usize].clone();
-        let previous_index: u32 = last_calculated_index[current_thread as usize].clone();
-        let target_index = tasks[current_thread as usize].pop_front().unwrap().clone();
-        threads_handles.push(thread::spawn(move ||{
-            calculate_a_n_from_previous(&previous_element,
-                                        &previous_index,
-                                        &target_index,
-                                        &default_precision,
-                                        sender.clone())
-        }));
-        calculated_elements += 1;
-    }
-
-    while calculated_elements < amount_of_elements as u32{
-        for current_thread in 0..thread_count{
-            match channels[current_thread as usize].try_recv(){
-                Ok(result_from_thread) => {
-                    result += result_from_thread.0.clone();
-                    last_calculated_index[current_thread as usize] = result_from_thread.1;
-                    elements[last_calculated_index[current_thread as usize] as usize] = result_from_thread.0.clone();
-
-                    calculated_elements += 1;
-
-                    let previous_a = elements[(current_thread * amount_of_work_per_thread) as usize].clone();
-                    let previous_index = last_calculated_index[current_thread as usize];
-                    match tasks[current_thread as usize].pop_front(){
-                        Some(next_task) =>{
-                            let (sender, receiver) = mpsc::channel();
-                            channels[current_thread] = receiver;
-                            threads_handles[current_thread as usize] = thread::spawn(move ||{
-                                calculate_a_n_from_previous(&previous_a,
-                                                            &previous_index,
-                                                            &next_task,
-                                                            &default_precision,
-                                                            sender.clone())
-                            })
-                        }
-                        None => continue
-                    }
-                }
-                Err(_) => continue
+    for start_task in (0..amount_of_elements).step_by(amount_of_work_per_thread){
+        let end_index: u32 = {
+            if start_task + amount_of_work_per_thread < amount_of_elements
+            {
+                start_task + amount_of_work_per_thread - 1
             }
+            else{
+                amount_of_elements - 1
+            }
+        } as u32;
+
+        task_tuples.push_back((start_task as u32, end_index))
+    }
+    let mut receivers: Vec<Receiver<Float>> = vec!();
+    let mut calculated_elements: u32 = 0;
+    for current_thread in 0..thread_count
+    {
+        let (sender, receiver) = mpsc::channel();
+        let (start, end) = task_tuples.pop_front().unwrap();
+        let mut current_task = Task::new(start, end, sender);
+        thread::spawn(move ||{
+            current_task.work();
+        });
+        receivers.push(receiver);
+    }
+    while calculated_elements < (amount_of_elements as u32)
+    {
+        for current_thread in 0..thread_count{
+            match receivers[current_thread].try_recv(){
+                Ok(result_from_thread) => {
+                    result += result_from_thread.clone();
+                    calculated_elements += amount_of_work_per_thread as u32;
+                    let (sender, receiver) = mpsc::channel();
+                    match task_tuples.pop_front(){
+                        Some((start, end)) => {
+                            let mut current_task = Task::new(start, end, sender);
+                            thread::spawn(move ||{
+                                current_task.work();
+                            });
+                            receivers[current_thread] = receiver;
+                        }
+                        None => {
+                            break;
+                        }
+                    };
+                },
+                Err(_) => continue,
+            };
         }
     }
+
+
+    /*let (sender, receiver) = mpsc::channel();
+
+    let mut current_task = Task::new(0, 100, sender);
+    let handle = thread::spawn(move ||{
+        current_task.work();
+    });
+
+    handle.join();
+    match receiver.try_recv(){
+        Ok(result_from_thread)=> result = result_from_thread,
+        Err(_) => {}
+    }*/
+
+
+
+    // let mut elements: Vec<Float> = vec![Float::new(DEFAULT_PRECISION); amount_of_elements as usize];
+    // let mut tasks_for_thread: Vec<VecDeque<u32>> = vec![VecDeque::new(); thread_count];
+    // let mut all_tasks: VecDeque<u32> = VecDeque::new();
+    //
+    // for i in 0..amount_of_elements {
+    //     all_tasks.push_back(i as u32);
+    // }
+    //
+    // let amount_of_work_per_thread = ((amount_of_elements as f32).ln() / (thread_count as f32).ln()) as usize;
+    // let mut last_calculated_index: Vec<u32> = vec![0; thread_count];
+    // let mut calculated_elements: u32 = 0;
+    //
+    // for current_thread in 0..thread_count {
+    //     for i in 0..amount_of_work_per_thread {
+    //         tasks_for_thread[current_thread as usize].push_back(all_tasks.pop_front().unwrap());
+    //     }
+    // }
+    //
+    // for current_thread in 0..thread_count {
+    //     elements[(amount_of_work_per_thread * current_thread) as usize] =
+    //         calculate_a_n_from_formula(tasks_for_thread[current_thread].pop_front() as u32, DEFAULT_PRECISION);
+    //
+    //     last_calculated_index[current_thread] = (amount_of_work_per_thread * current_thread) as u32;
+    //     calculated_elements += 1;
+    //     result += elements[(amount_of_work_per_thread * current_thread) as usize].clone();
+    // }
+    //
+    //
+    // let mut threads_handles: Vec<JoinHandle<()>> = vec!();
+    // let mut channels: Vec<mpsc::Receiver<(Float, u32)>> = vec!();
+    //
+    // for current_thread in 0..thread_count {
+    //     let current_task = Task::new(
+    //         (elements[(current_thread * amount_of_work_per_thread) as usize]).clone(),
+    //         last_calculated_index[current_thread as usize],
+    //         tasks_for_thread[current_thread as usize].pop_front().unwrap(),
+    //     );
+    //     channels.push(current_task.receiver);
+    //     threads_handles.push(current_task.start_thread());
+    //     calculated_elements += 1;
+    // }
+    //
+    // while calculated_elements < amount_of_elements as u32 {
+    //     for current_thread in 0..thread_count {
+    //         match channels[current_thread as usize].try_recv() {
+    //             Ok(result_from_thread) => {
+    //                 result += result_from_thread.0.clone();
+    //                 last_calculated_index[current_thread as usize] = result_from_thread.1;
+    //                 elements[last_calculated_index[current_thread as usize] as usize] = result_from_thread.0.clone();
+    //
+    //                 let next_task: u32 = match tasks_for_thread[current_thread as usize].pop_front() {
+    //                     Some(next_task) => next_task,
+    //                     None => {
+    //                         for i in 0..amount_of_work_per_thread {
+    //                             match all_tasks.pop_front() {
+    //                                 Some(next_task) => {
+    //                                     tasks_for_thread[current_thread].push_back(next_task);
+    //                                 }
+    //                                 None => break
+    //                             }
+    //                         }
+    //                     }
+    //                 };
+    //
+    //                 let current_task = Task::new(
+    //                     elements[(current_thread * amount_of_work_per_thread) as usize].clone(),
+    //                     last_calculated_index[current_thread as usize],
+    //                     next_task,
+    //                 );
+    //                 channels[current_thread] = current_task.receiver;
+    //                 threads_handles[current_thread as usize] = current_task.start_thread();
+    //                 calculated_elements += 1;
+    //             }
+    //             Err(_) => continue
+    //         }
+    //     }
+    // }
 
     println!("{}", 1 / result);
 }
