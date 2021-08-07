35fb1fb0fde1a3452dc2d2ce0f8684f46830134b
lyubolp <lyubo.karev@gmail.com>
2021-08-07 08:32:40 +0300

    Changed time_run.sh parameter

--- time_run.sh
+++ time_run.sh
@@ -2,6 +2,6 @@
 
 for turn in $(seq 1 $3)
 do
-  result=$(/usr/bin/time -f "%E" ./target/release/multithreaded_pi_rust -p $1 -t $2 2>&1 > /dev/null);
+  result=$(/usr/bin/time -f "%E" ./target/release/multithreaded_pi_rust -e $1 -t $2 2>&1 > /dev/null);
   echo $result
 done
